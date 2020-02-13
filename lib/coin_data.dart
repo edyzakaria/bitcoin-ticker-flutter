4ebd4ce9e28708c0300e71f9d6f33840140766fc
edyzakaria@gmail.com <edyzakaria@gmail.com>
2020-02-13 20:59:27 +0700

    Finalizing bit coin app.

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:bitcoin_ticker/coin_data.dart';
+
 const List<String> currenciesList = [
   'AUD',
   'BRL',
@@ -28,4 +32,36 @@ const List<String> cryptoList = [
   'LTC',
 ];
 
-class CoinData {}
+const coinURL = 'https://rest.coinapi.io/v1/exchangerate';
+const coinAPIKey = '9AA66FE3-FFEB-4261-958C-9FD4BE907852';
+
+class CoinData {
+  CoinData ();
+
+  String _coinName;
+  String _currency;
+
+  Future<dynamic> getCoinPrice ({String currency}) async {
+    Map<String, String> cryptoPrices = {};
+    this._currency = currency;
+    for (String crypto in cryptoList) {
+      this._coinName = crypto;
+      String url = '$coinURL/$_coinName/$_currency?apikey=$coinAPIKey';
+      http.Response response = await http.get(url).timeout(Duration(seconds: 20));
+      if (response.statusCode == 200) {
+        String data = response.body;
+        double rate = jsonDecode(data)['rate'];
+        cryptoPrices[crypto] = '1 ${this._coinName} = ${rate.toStringAsFixed(2)} ${this._currency}';
+        print(cryptoPrices[crypto]);
+      } else {
+        print('Calling for $url get ${response.statusCode}');
+//        throw('Error getting prices.');
+        cryptoPrices[crypto] = '1 ${this._coinName} = 999 ${this._currency}';
+      }
+    }
+  return cryptoPrices;
+  }
+
+
+
+}
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -9,10 +11,34 @@ class PriceScreen extends StatefulWidget {
 class _PriceScreenState extends State<PriceScreen> {
 
   String selectedValue = 'USD';
+  Map<String, String> coinPriceTag = {};
+  String bitCoinPriceTag = 'Getting price...';
+  CoinData coinData = CoinData();
 
-  List <DropdownMenuItem<String>> getDropDownItems () {
+  @override
+  void initState() {
+    super.initState();
+    updateCoin();
+  }
+
+  void updateCoin() async {
+    for (String crypto in cryptoList) {
+      coinPriceTag[crypto] = 'Getting prices...';
+    }
+    try {
+      var data = await coinData.getCoinPrice(currency: selectedValue);
+      setState(() {
+        coinPriceTag = data;
+        print(coinPriceTag);
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  DropdownButton<String> androidDropDown () {
+
     List <DropdownMenuItem<String>> dropDownItems = [];
-
     for(String currency in currenciesList) {
       var newItem = DropdownMenuItem (
         child: Text(currency),
@@ -20,7 +46,64 @@ class _PriceScreenState extends State<PriceScreen> {
       );
       dropDownItems.add(newItem);
     }
-    return dropDownItems;
+
+    return DropdownButton <String> (
+        value: selectedValue,
+        items: dropDownItems,
+        onChanged: (value) {
+          setState(() {
+            selectedValue = value;
+            updateCoin();
+          });
+        });
+  }
+
+  CupertinoPicker iosPicker() {
+
+    List<Text> pickerItems = [];
+    for (String currency in currenciesList) {
+      pickerItems.add(Text(currency));
+    }
+
+    return CupertinoPicker(
+      backgroundColor: Colors.lightBlue,
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
+  List<Widget> coinComponents() {
+    List<Widget> padList = [];
+    for (String crypto in cryptoList) {
+//      bool priceIsNull = false;
+//      if (coinPriceTag[crypto] == null) {priceIsNull = true;}
+      var padItem = Padding(
+        padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+        child: Card(
+          color: Colors.lightBlueAccent,
+          elevation: 5.0,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
+            child: Text(
+              coinPriceTag[crypto],
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        ),
+      );
+      padList.add(padItem);
+    }
+    return padList;
   }
 
   @override
@@ -33,43 +116,20 @@ class _PriceScreenState extends State<PriceScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-            ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: coinComponents(),
           ),
           Container(
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: DropdownButton <String> (
-                value: selectedValue,
-                items: getDropDownItems(),
-              onChanged: (value) {
-              setState(() {
-                selectedValue = value;
-              });
-            }),
+            child: Platform.isIOS ? iosPicker() : androidDropDown(),
           ),
         ],
       ),
     );
   }
 }
+
